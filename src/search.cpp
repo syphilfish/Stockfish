26a00d1008a67e3b62cacbae0283166e01e7b579
syphilfish <gracie34634@gmail.com>
2026-02-20 17:05:46 -0800

    correctionValue qs

--- src/search.cpp
+++ src/search.cpp
@@ -1599,7 +1599,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
         if (bestValue > alpha)
             alpha = bestValue;
 
-        futilityBase = ss->staticEval + 351;
+        futilityBase = ss->staticEval + 351 + std::abs(correctionValue) / 262144;
     }
 
     const PieceToHistory* contHist[] = {(ss - 1)->continuationHistory};
