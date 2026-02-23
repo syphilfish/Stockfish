5984cc5c3212930ce643b58f621d1c48bd93b62b
syphilfish <gracie34634@gmail.com>
2026-02-23 02:01:33 -0800

    d += ext < 2

--- src/search.cpp
+++ src/search.cpp
@@ -1146,7 +1146,7 @@ moves_loop:  // When in check, search starts here
                 extension =
                   1 + (value < singularBeta - doubleMargin) + (value < singularBeta - tripleMargin);
 
-                depth++;
+                depth += (extension < 2);
             }
 
             // Multi-cut pruning
