3ca63fedff89beacdd1a824dc5bef72aa2055bc7
syphilfish <gracie34634@gmail.com>
2026-05-25 19:12:17 -0700

    nullD + stdmin max int eval - beta 196 2 bench: 2553070

--- src/search.cpp
+++ src/search.cpp
@@ -956,7 +956,8 @@ Value Search::Worker::search(
         assert((ss - 1)->currentMove != Move::null());
 
         // Null move dynamic reduction based on depth
-        Depth R = 7 + depth / 3;
+        Depth R = 7 + depth / 3
+        + std::min(std::max(int(eval - beta), 0) / 196, 2);
         do_null_move(pos, st, ss);
 
         Value nullValue = -search<NonPV>(pos, ss + 1, -beta, -beta + 1, depth - R, false);
