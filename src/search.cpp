fb79e054e68ea74e7288937ae0269838b95907f6
syphilfish <gracie34634@gmail.com>
2026-05-28 15:39:41 -0700

    probCutB - 24 * std min ttd depth 0 6 bench: 2654968

--- src/search.cpp
+++ src/search.cpp
@@ -1043,7 +1043,7 @@ Value Search::Worker::search(
 moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea
-    probCutBeta = beta + 428;
+    probCutBeta = beta + 428 - 24 * std::min(std::max(ttData.depth - depth, 0), 6);
     if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
         && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
         return probCutBeta;
