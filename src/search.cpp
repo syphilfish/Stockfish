52f3458cccf4e70d88e4afbe4bee697348944180
syphilfish <gracie34634@gmail.com>
2026-05-29 17:23:14 -0700

    nmp imp - 24 * oppWorse + 374 bench: 3307672

--- src/search.cpp
+++ src/search.cpp
@@ -950,8 +950,9 @@ Value Search::Worker::search(
     }
 
     // Step 9. Null move search with verification search
-    if (cutNode && ss->staticEval >= beta - 14 * depth - 45 * improving + 374 && !excludedMove
-        && pos.non_pawn_material(us) && ss->ply >= nmpMinPly && !is_loss(beta))
+    if (cutNode
+        && ss->staticEval >= beta - 14 * depth - 45 * improving - 24 * opponentWorsening + 374
+        && !excludedMove && pos.non_pawn_material(us) && ss->ply >= nmpMinPly && !is_loss(beta))
     {
         assert((ss - 1)->currentMove != Move::null());
 
